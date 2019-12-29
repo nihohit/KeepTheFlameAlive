29a1e59cb90204cbc1f0d6de18e07b037d70267e
Shachar Langbeheim <shachar@lightricks.com>
2019-12-29 19:12:48 +0200

    Update fields scores.

---
+++ FlameAlive/Assets/scripts/FieldScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldScript : MonoBehaviour {
+    public GameObject textObject;
+    public string headerText;
+    private UnityEngine.UI.Text textField;
+    private int numberOfCollidingClouds = 0;
+    private long score;
+
+    // Start is called before the first frame update
+    void Start() {
+        textField = textObject.GetComponent<UnityEngine.UI.Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        score += (numberOfCollidingClouds * 2) - 3;
+        textField.text = headerText + " " + score / 10;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("collision enter");
+        numberOfCollidingClouds++;
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("collision exit");
+        numberOfCollidingClouds--;
+    }
+}
