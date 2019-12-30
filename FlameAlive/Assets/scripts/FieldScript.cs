48d13dfb39f2412ece70065e19de81866c9c7e7f
Shachar Langbeheim <shachar@lightricks.com>
2019-12-30 11:50:39 +0200

    Added explosion when growable bursts.

--- FlameAlive/Assets/scripts/FieldScript.cs
+++ FlameAlive/Assets/scripts/FieldScript.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldScript : MonoBehaviour {
     public GameObject textObject;
+    public GameObject growable;
     public string headerText;
     public int gainPerUpdate;
     public int lossPerUpdate;
     private UnityEngine.UI.Text textField;
     private int numberOfCollidingClouds = 0;
-    private long score;
+    private float score;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,7 +21,18 @@ public class FieldScript : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         score += numberOfCollidingClouds > 0 ? gainPerUpdate : -lossPerUpdate;
+        score = Mathf.Max(score, 0f);
         textField.text = headerText + " " + score / 10;
+        growable.transform.localScale = new Vector3(score / 100, score / 100, score / 100);
+        if (score > 500) {
+            popGrowable();
+        }
+    }
+
+    void popGrowable() {
+        score = 0;
+        var effect = growable.GetComponentInChildren<ParticleSystem>();
+        effect.Play();
     }
 
     void OnTriggerEnter(Collider other) {
