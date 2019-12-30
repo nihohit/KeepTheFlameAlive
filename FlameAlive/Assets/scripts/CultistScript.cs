122d983a0b0b47a178eb9e7bbf0205fc87dd2cf7
Shachar Langbeheim <shachar@lightricks.com>
2019-12-30 14:11:08 +0200

    Rotate around the fire.

--- FlameAlive/Assets/scripts/CultistScript.cs
+++ FlameAlive/Assets/scripts/CultistScript.cs
@@ -8,24 +8,28 @@ public class CultistScript : MonoBehaviour {
     Vector3 target = new Vector3(0.890671f, 0, 0.403903f);
     float initialDistance;
     float radius = 0.7f;
-    float angleChangeSpeed = (2 * Mathf.PI) / 5;
+    float angleChangeSpeed;
 
     // Start is called before the first frame update
     void Start() {
+        angleChangeSpeed = angleChangeSpeed = (2 * Mathf.PI) / 5 * Mathf.Rad2Deg;
         initialPosition = transform.position;
         initialDistance = Vector3.Distance(target, initialPosition);
     }
 
     // Update is called once per frame
     void Update() {
+        transform.LookAt(target);
         var distanceFromTarget = Vector3.Distance(transform.position, target);
         if (distanceFromTarget <= radius) {
-            var angleChange = angleChangeSpeed * Time.deltaTime;
-            var angle = angleChange + Vector3.Angle(target + Vector3.left, transform.position);
-            transform.position = target + new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+            transform.RotateAround(target, Vector3.up, angleChangeSpeed * Time.deltaTime);
         } else {
             var ratio = (initialDistance - distanceFromTarget + Time.deltaTime) / initialDistance;
             transform.position = Vector3.Lerp(initialPosition, target, ratio);
         }
+    }
+
+    Vector3 difference(float angle) {
+        return new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
     }
 }
