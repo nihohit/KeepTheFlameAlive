03bd32961a0896daeeb4e115323a6f2ec2a75b87
Shachar Langbeheim <shachar@lightricks.com>
2019-12-30 12:45:37 +0200

    Added cultist prefab.

---
+++ FlameAlive/Assets/scripts/CultistScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CultistScript : MonoBehaviour {
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        // agent.
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Vector3.Distance(transform.position, Vector3.zero) <= 1.7) {
+
+        } else {
+            transform.position = Vector3.Lerp(transform.position, Vector3.zero, Time.deltaTime);
+        }
+    }
+}
